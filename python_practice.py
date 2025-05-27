cdef8d277e5efc18ac413851b78b89b36d4bc200
samira7165 <141204267+samira7165@users.noreply.github.com>
2025-05-27 23:54:30 +0600

    Add files via upload

---
+++ python_practice.py
@@ -0,0 +1,17 @@
+name= 'SAMIRA'
+print('My name is ' +name)
+num = 34
+print('My age is ',num)
+print(bin(3))
+
+
+"""
+sentence=input('Enter a sentence: ')
+print("Your sentence is: "+sentence)
+word1=input('write the word you want to replace: ')
+word2=input('Write the word you want to replace with: ')
+print(sentence.replace(word1,word2))
+"""
+
+list1=['banana','lichi','mango']
+print(list1.count('banana'))
