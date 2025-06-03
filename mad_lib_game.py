247a655d0ce660bfb7fea42ab4274ac4722ddfc0
samira7165 <141204267+samira7165@users.noreply.github.com>
2025-06-03 22:20:33 +0600

    Add files via upload

---
+++ mad_lib_game.py
@@ -0,0 +1,21 @@
+adj1=input("Enter a adjective: ")
+adj2=input("Enter a adjective: ")
+adj3=input("Enter a adjective ")
+adj4=input("Enter a adjective: ")
+adj5=input("Enter a adjective: ")
+verb1=input("Enter a verb: ")
+noun1=input("Enter a noun: ")
+noun2=input("Enter a noun: ")
+noun5=input("Enter a noun: ")
+noun6=input("Enter a noun: ")
+
+
+print(f"Today was a really {adj1} day. I woke up feeling {adj2} and decided to wear my favorite{adj3} shirt")
+
+print(f"On the way to the market, I saw a {adj4} {noun1} dancing on top of a {noun2}.")
+print(f"It {verb1} so loudly that even a {adj5} noun3 dropped its groceries!")
+
+
+
+print(f"Suddenly, a {noun5} ran past me, yelling, “exclamation1! The {noun6} is melting!”")
+
